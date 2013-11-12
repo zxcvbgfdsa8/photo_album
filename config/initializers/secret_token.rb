ff575bf0c940010ea45a6fbcd0e71d79e6378004
James Patrick West <zxcvbgfdsa8@gmail.com>
2013-11-12 16:56:32 -0700

    initial build

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoAlbum::Application.config.secret_key_base = '46cd59caaee7ca03136122e26117b6d9b8e90106796ffb784a77543b5f22972286b162880ad6ba5f0091ddfc9212bd7929581929b8b8a12c12900550d5e1e7fc'
